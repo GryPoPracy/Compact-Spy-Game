c2fdb6104232a6995507a7b22067d18d5c583dd1
Ukidabek <ukidabek@gmail.com>
2018-07-22 16:00:33 +0200

    - Camera bounds logic added.

--- Assets/Content/Code/GameLogic/Character/State/CameraLocomotionState2D.cs
+++ Assets/Content/Code/GameLogic/Character/State/CameraLocomotionState2D.cs
@@ -1,22 +1,30 @@
 ﻿using BaseGameLogic.States;
+using BaseGameLogic.Utilities;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class CameraLocomotionState2D : IState, IOnLateUpdate
 {
+    [RequiredReference] private Camera _camera = null;
     [RequiredReference] private Transform _transform = null;
     private Transform PlayerTransform { get { return PlayerCharacter.Instance.transform; } }
     [RequiredReference] private CameraLocomotionState2DSettings _cameraLocomotionState2DSettings = null;
 
+    private OrthogtaphicCameraBoundsConstraint constraint = null;
+
     public CameraLocomotionState2D() {}
     
-    public void OnEnter() {}
+    public void OnEnter()
+    {
+        constraint = new OrthogtaphicCameraBoundsConstraint(_cameraLocomotionState2DSettings.Bounds, _camera);
+    }
 
     public void OnExit() {}
 
     public void OnLateUpdate()
     {
+        float x = _camera.orthographicSize * _camera.aspect;
         Vector3 cameraPosition = _transform.position;
         cameraPosition.z = 0;
         float distance = Vector3.Distance(cameraPosition, PlayerTransform.position);
@@ -32,5 +40,7 @@ public class CameraLocomotionState2D : IState, IOnLateUpdate
             _transform.position, 
             PlayerTransform.position + _cameraLocomotionState2DSettings.CameraOffset, 
             _cameraLocomotionState2DSettings.Speed * Time.deltaTime * multimultiplier);
+        if(_cameraLocomotionState2DSettings.UseBounds)
+            constraint.ForceBoundaries();
     }
 }
--- Assets/Content/Code/GameLogic/Character/State/CameraLocomotionState2DSettings.cs
+++ Assets/Content/Code/GameLogic/Character/State/CameraLocomotionState2DSettings.cs
@@ -1,8 +1,9 @@
-﻿using System.Collections;
+﻿using BaseGameLogic.Utilities;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CameraLocomotionState2DSettings : MonoBehaviour
+public partial class CameraLocomotionState2DSettings : MonoBehaviour
 {
     [SerializeField] private float _speed = 1;
     public float Speed { get { return _speed; } }
@@ -16,5 +17,18 @@ public class CameraLocomotionState2DSettings : MonoBehaviour
     [SerializeField] private float _teleportDistance = 4f;
     public float TeleportDistance { get { return _teleportDistance; } }
 
+    [SerializeField] private bool _useBounds = true;
+    public bool UseBounds { get { return _useBounds; } }
+    [SerializeField] private OrthogtaphicCameraBounds _bounds = new OrthogtaphicCameraBounds();
+    public OrthogtaphicCameraBounds Bounds { get { return _bounds; } }
 
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawLine(new Vector3(_bounds.MinWidth, _bounds.MaxHeight, 0f), new Vector3(_bounds.MaxWidth, _bounds.MaxHeight, 0f));
+        Gizmos.DrawLine(new Vector3(_bounds.MaxWidth, _bounds.MaxHeight, 0f), new Vector3(_bounds.MaxWidth, _bounds.MinHeight, 0f));
+        Gizmos.DrawLine(new Vector3(_bounds.MinWidth, _bounds.MaxHeight, 0f), new Vector3(_bounds.MinWidth, _bounds.MinHeight, 0f));
+        Gizmos.DrawLine(new Vector3(_bounds.MinWidth, _bounds.MinHeight, 0f), new Vector3(_bounds.MaxWidth, _bounds.MinHeight, 0f));
+    }
 }
