289c9554d27258b882a2568b0e2d39d2a818dd38
Ukidabek <ukidabek@gmail.com>
2018-08-05 18:55:33 +0200

    - Skills refactor.

--- Assets/Content/Code/GameLogic/Skills/Skill.cs
+++ Assets/Content/Code/GameLogic/Skills/Skill.cs
@@ -9,36 +9,11 @@ namespace Skills
 {
     public class Skill : MonoBehaviour
     {
-        [SerializeField] private float _countDownTime = 1f;
-        private float _countDownCounter = 0;
         [SerializeField] private ActionList _useSkillActionList = new ActionList();
-        [SerializeField] private ActionList _restoreSkillActionList = new ActionList();
-
-        public CountdownCallback CountdownCallback = new CountdownCallback();
 
         public void Perform()
         {
-            if (_countDownCounter > 0)
-                return;
-
             _useSkillActionList.Perform();
-            StartCoroutine(CountdownCorutine());
-        }
-
-        private IEnumerator CountdownCorutine()
-        {
-            _countDownCounter = _countDownTime;
-
-            while (_countDownCounter > 0)
-            {
-                _countDownCounter -= Time.deltaTime;
-                CountdownCallback.Invoke(1 - (_countDownCounter / _countDownTime));
-                yield return null;
-            }
-
-            _restoreSkillActionList.Perform();
         }
     }
-
-    [Serializable] public class CountdownCallback : UnityEvent<float> {}
 }
