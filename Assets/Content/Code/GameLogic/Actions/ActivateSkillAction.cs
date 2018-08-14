3903f6e6bb2b2cf86cf8d48a3372d9eab61ce8aa
T.Bentkowski <tombentkowski@gmail.com>
2018-08-14 23:30:41 +0200

    Added sprites and prefab maker scene

---
+++ Assets/Content/Code/GameLogic/Actions/ActivateSkillAction.cs
@@ -0,0 +1,15 @@
+﻿using ActionsSystem;
+using Skills;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ActivateSkillAction : BaseAction
+{
+    [SerializeField] private string _name = string.Empty;
+
+    public override void Perform(params object[] list)
+    {
+        SkilsManager.Instance.PerformSkill(_name);
+    }
+}
---
+++ Assets/Content/Code/GameLogic/Skills/Skill.cs
@@ -0,0 +1,20 @@
+﻿using ActionsSystem;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+namespace Skills
+{
+    public class Skill : MonoBehaviour
+    { 
+        [SerializeField] private ActionList _useSkillActionList = new ActionList();
+
+        public void Perform()
+        {
+            SkilsManager.Instance.PerformSkill(gameObject.name);
+            _useSkillActionList.Perform();
+        }
+    }
+}
