853f434bd0afbeb2c279e54e0b867e98ffeb7920
Ukidabek <ukidabek@gmail.com>
2018-08-15 15:09:11 +0200

    - Invisibility fix.

---
+++ Assets/Content/Code/GameLogic/Skills/DummyEffect.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DummyEffect : IEffect
+{
+    public void Activate(GameObject target)
+    {
+    }
+
+    public void Deactivate()
+    {
+    }
+
+    public bool Update(float deltaTime)
+    {
+        return false;
+    }
+}
